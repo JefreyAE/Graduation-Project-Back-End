0da596c5b1c817a1eb864c5e489b089cdce76422
JefreyAE <47003720+JefreyAE@users.noreply.github.com>
2021-08-03 14:14:57 -0600

    Version 1.2

---
+++ app/Helpers/DateHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Helpers;
+
+use Carbon\Carbon;
+
+class DateHelper{
+
+    function getAge($date){
+            $now = Carbon::now();
+            $years = $now->diffInYears($date, $now);
+            $months = $now->diffInMonths($date, $now);
+            $days = $now->diffInDays($date, $now);
+    
+            $months = $months-$years*12;
+            $days = $days-$months*30;
+    
+            $age = $years.' años, '.$months.' meses, y '.$days.' días aproximadamente';
+            return $age;
+        }
+}
